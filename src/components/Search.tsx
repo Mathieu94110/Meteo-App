e92682dca99633f035eaa5af9920c7bc7d919cdd
Mathieu Enault <me94110@gmail.com>
2020-09-22 22:50:54 +0200

    adding Seach component  content

---
+++ src/components/Search.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { FormEvent, useState } from "react";
+import { setAlert } from "../store/actions/alertActions";
+import { getMeteo, setLoading } from "../store/actions/weatherActions";
+
+interface SearchProps {
+  title: string;
+}
+
+const Search: React.FC<SearchProps> = ({ title }) => {
+  const [city, setCity] = useState("");
+  const dispatch = useDispatch();
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    setCity(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (city.trim() === "") {
+      return dispatch(setAlert("Vous devez renseignez une ville !"));
+    }
+    dispatch(setLoading());
+    dispatch(getMeteo(city));
+    setCity("");
+  };
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={city}
+          placeholder="choisissez une ville !"
+        />
+        <button></button>
+      </form>
+    </div>
+  );
+};
+
+export default Search;
